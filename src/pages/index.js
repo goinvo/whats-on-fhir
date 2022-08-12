28fae36804d40d3b39b6919f8b3a74dc4c0af104
Craig McGinley <craigmcginley@gmail.com>
2022-08-12 13:07:53 -0400

    content tweaks, spelling corrections, clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,6 @@ import Layout from '../components/layout.js'
 import GlossaryItem from '../components/glossary-item.js'
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
 const containerStyles = {
   maxWidth: 600,
   margin: '0 auto',
@@ -27,7 +23,7 @@ const hrStyles = {
   borderRight: 0,
   margin: '1rem 0',
 }
-const listItemStyles = {
+const cardStyles = {
   padding: '2rem',
   marginBottom: '3rem',
   border: '1px solid #bababa',
@@ -42,7 +38,7 @@ const prompts = [
   {
     verb: "is",
     title: "a framework.",
-    description: "A very high level, or abstracted framework, that is. Some may call it a platform. FHIR provides very generalized building blocks to represent heath data in a variety of settings and applications. It provides avenues for these building blocks to be configured and customized to meet specific needs. On top of that, it provides guidance on how these building blocks should be communicated back and forth between systems across the internet. To understand this better, see the 'FHIR analogies' page. The abtract or 'meta' nature of FHIR means..."
+    description: "A very high level, or abstracted framework, that is. Some may call it a platform. FHIR provides very generalized building blocks to represent heath data in a variety of settings and applications. It provides avenues for these building blocks to be configured and customized to meet specific needs. On top of that, it provides guidance on how these building blocks should be communicated back and forth between systems across the internet. To understand this better, see the 'FHIR analogies' page. The abstract or 'meta' nature of FHIR means..."
   },
   {
     verb: "is",
@@ -55,7 +51,7 @@ const prompts = [
   {
     verb: "is",
     title: "complicated.",
-    description: "The framework is purposefully a bit ambiguously. Since FHIR doesn't directly prescribe specific usage or data standardization, it can be hard to digest the various documentation and learning resources. Even the naming of concepts within FHIR can be ambiguous, like a 'Resource' or Use Case/'Implementation'. In order to adequately describe the purposes and uses of a system this broad and generalized, it requires a lot of documentation, which ends up being spread across disperate locations. In fact, FHIR is so complicated that its maintainers and community members are constantly active in a dedicated FHIR chat application, as they expect there to be plenty of confusion and questions. And all this is assuming moderate experience with software. For many clinicians, patients, even non-healthcare oriented software engineers, FHIR is intangible. On top of being a bit tough to grasp..."
+    description: "The framework is purposefully a bit ambiguously. Since FHIR doesn't directly prescribe specific usage or data standardization, it can be hard to digest the various documentation and learning resources. Even the naming of concepts within FHIR can be ambiguous, like a 'Resource' or Use Case/'Implementation'. In order to adequately describe the purposes and uses of a system this broad and generalized, it requires a lot of documentation, which ends up being spread across disparate locations. In fact, FHIR is so complicated that its maintainers and community members are constantly active in a dedicated FHIR chat application, as they expect there to be plenty of confusion and questions. And all this is assuming moderate experience with software. For many clinicians, patients, even non-healthcare oriented software engineers, FHIR is intangible. On top of being a bit tough to grasp..."
   },
   {
     verb: "is",
@@ -74,13 +70,13 @@ const prompts = [
   },
   {
     verb: "does",
-    title: "move HealthIT to APIs.",
+    title: "move Health IT to APIs.",
     description: "Much of the focus of FHIR is to facilitate use of already well established electronic methods of communication across the internet, like the use of web-based APIs. The basic concept being web APIs allow computer systems to talk to each other in order to add, edit, delete, retrieve, and search data. For instance, with FHIR enabled APIs, a system could theoretically ask for a list of all patients with diabetes, or tell another system when a patient's address changes."
   },
   {
     verb: "does",
     title: "reduce excessive and cluttered exchange.",
-    description: "Eletronic health data exchange to date has tended to be bulky and messy. Two systems that agree to share data may oftentimes dump all their data to eachother, including data that may not be necessary for the others' use. It can also include unformatted, raw, or unstructured data, making it difficult for the receiver of the data to accurately identify or utilize it. FHIR moves health data exchange to smaller bites, with the ability to have finer control over what is requested and what is delivered, thus providing better privacy, security, and efficiency."
+    description: "Electronic health data exchange to date has tended to be bulky and messy. Two systems that agree to share data may oftentimes dump all their data to each-other, including data that may not be necessary for the others' use. It can also include unformatted, raw, or unstructured data, making it difficult for the receiver of the data to accurately identify or utilize it. FHIR moves health data exchange to smaller bites, with the ability to have finer control over what is requested and what is delivered, thus providing better privacy, security, and efficiency."
   },
   {
     verb: "doesn't",
@@ -95,7 +91,12 @@ const prompts = [
   {
     verb: "doesn't",
     title: "solve interoperability.",
-    description: "In addtion to above, there are some other barriers to true interoperabiltiy that FHIR does not just magically 'solve'. It doesn't automatically give you infrastructure or security requirements out of the box, and it still takes a significant amount of effort (time and money) to implement FHIR. In addition, exchanging of patient data across systems can easily lead to duplicates, multiple 'versions' of the same patient in the database. While FHIR, outlines methods to interact with services to match patient duplicates together (called 'patient matching'), it does not provide the service itself. Overall, FHIR is still a huge foundational step towards interoperability, but it doesn't, and shouldn't, do 'all the things'."
+    description: "In addition to above, there are some other barriers to true interoperability that FHIR does not just magically 'solve'. It doesn't automatically give you infrastructure or security requirements out of the box, and it still takes a significant amount of effort (time and money) to implement FHIR. In addition, exchanging of patient data across systems can easily lead to duplicates, multiple 'versions' of the same patient in the database. While FHIR, outlines methods to interact with services to match patient duplicates together (called 'patient matching'), it does not provide the service itself. FHIR aims to provide solutions to many interoperability issues, but it doesn't, and shouldn't, do 'all the things'."
+  },
+  {
+    verb: "<i>does</i>",
+    title: "improve the health data ecosystem.",
+    description: "While FHIR isn't a panacea to solve all of interoperability, it's a huge foundational step towards the future. The adoption and evolution of FHIR greatly improves the portability, breadth, and quality of electronic health data. The vision of FHIR is backed by US government health organizations including CMS, ONC, and HHS. National adoption is growing alongside policy. Beyond the US, it opens avenues to the entire world to share health data, improve data ownership, improve population health and research, and reach better health outcomes."
   },
 ]
 
@@ -103,13 +104,13 @@ const prompts = [
 const IndexPage = () => {
   return (
     <Layout>
-      <main style={pageStyles}>
+      <main>
         <title>What's on FHIR?</title>
         <div style={containerStyles}>
           <Heading as='h1' size='2xl' style={{textAlign: 'center', marginTop: '2rem', marginBottom: '3rem' }}>What's on FHIR?</Heading>
           {prompts.map(prompt => (
-            <div key={prompt.title} style={{ ...listItemStyles }}>
-              <Heading as='h4' size='sm' style={headingStyles}>FHIR {prompt.verb}...</Heading>
+            <div key={prompt.title} style={{ ...cardStyles }}>
+              <Heading as='h4' size='sm' style={headingStyles}>FHIR <JsxParser renderInWrapper={false} jsx={prompt.verb} />...</Heading>
               <Heading as='h2' size='lg' style={headingStyles}>{prompt.title}</Heading>
               <hr style={hrStyles} />
               <p style={descriptionStyle}>
